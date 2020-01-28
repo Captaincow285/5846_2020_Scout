f804732aab7a93e469de16d3e6bf80b1b1f827e9
Captaincow285 <Captaincow285@gmail.com>
2020-01-28 18:12:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a5846_2020_scout/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.a5846_2020_scout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Intent option1 = new Intent(getApplicationContext(), MatchActivityDef.class);
+
+        int sel1Id = 10011;
+        int sel2Id = 10012;
+        int sel3Id = 10013;
+        int sel4Id = 10014;
+        RadioButton initSel1 = findViewById(R.id.initSel1);
+        initSel1.setId(sel1Id);
+        RadioButton initSel2 = findViewById(R.id.initSel2);
+        initSel2.setId(sel2Id);
+        RadioButton initSel3 = findViewById(R.id.initSel3);
+        initSel3.setId(sel3Id);
+        RadioButton initSel4 = findViewById(R.id.initSel4);
+        initSel4.setId(sel4Id);
+
+        Button firstExec = findViewById(R.id.initExecButton);
+        firstExec.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                RadioGroup firstSet = findViewById(R.id.initSelectGroup);
+                int radioSelect = firstSet.getCheckedRadioButtonId();
+                if(!(radioSelect == -1))
+                {
+                    Log.d("Working", "Filled RadioButton");
+                    if(radioSelect == 10011)
+                    {
+                        startActivity(option1);
+                    }
+                    else if(radioSelect == 10012)
+                    {
+                        Log.d("Unimplemented", "Set Competition Selected");
+                    }
+                    else if(radioSelect == 10013)
+                    {
+                        Log.d("Unimplemented","Download Data to PC Selected");
+                    }
+                    else if(radioSelect == 10014)
+                    {
+                        Log.d("Unimplemented","How to Use This App Selected");
+                    }
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/a5846_2020_scout/MatchActivityDef.java
@@ -0,0 +1,75 @@
+package com.example.a5846_2020_scout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MatchActivityDef extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceStat) {
+        super.onCreate(savedInstanceStat);
+        setContentView(R.layout.activity_match_pre);
+        Log.d("Working", "MatchActivityDef Works!");
+        final Intent returnToMain = new Intent(getApplicationContext(), MainActivity.class);
+
+        Switch pracToggle = findViewById(R.id.pracToggle);
+        Button preScoutSubmit = findViewById(R.id.preScoutSubmit);
+        Button returnButton = findViewById(R.id.returnButton);
+        final TextView matchNumBox = findViewById(R.id.matchNumBox);
+        final EditText matchNumInput = findViewById(R.id.matchNumberInput);
+        final TextView scoutInitialsBox = findViewById(R.id.scoutInitialsBox);
+        final EditText initialsInput = findViewById(R.id.initialsInput);
+        final TextView pracModeText = findViewById(R.id.pracModeText);
+        final TextView pracModeText2 = findViewById(R.id.pracModeText2);
+
+        pracModeText.setVisibility(View.GONE);
+        pracModeText2.setVisibility(View.GONE);
+
+        pracToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    matchNumBox.setVisibility(View.GONE);
+                    matchNumInput.setVisibility(View.GONE);
+                    scoutInitialsBox.setVisibility(View.GONE);
+                    initialsInput.setVisibility(View.GONE);
+                    pracModeText.setVisibility(View.VISIBLE);
+                    pracModeText2.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    matchNumBox.setVisibility(View.VISIBLE);
+                    matchNumInput.setVisibility(View.VISIBLE);
+                    scoutInitialsBox.setVisibility(View.VISIBLE);
+                    initialsInput.setVisibility(View.VISIBLE);
+                    pracModeText.setVisibility(View.GONE);
+                    pracModeText2.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        preScoutSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(returnToMain);
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(returnToMain);
+            }
+        });
+    }
+}
